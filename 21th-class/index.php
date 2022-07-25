61bb8f4cd79c15bb84c5fae0e1026a4c76aebada
rofikul009 <rofikulislam009@gmail.com>
2022-07-25 09:59:01 -0700

    commit

--- 21th-class/index.php
+++ 21th-class/index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
 </head>
 <body>
+    <h2>good</h2>
+    <div class="container">
+        <h1>Rofikul</h1>
+        <?php
+            $x = 10;
+            $y = 20;
+            function multipication(){
+                $mul = $GLOBALS['x'] * $GLOBALS['y'];
+                echo $mul;
+            }
+            multipication();
+        
+        
+        
+        ?>
+
+    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+   
     
 </body>
 </html>
